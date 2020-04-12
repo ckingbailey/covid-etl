df5c89a7dd5c7fd706b8bce13ca6ad018d8f4c7e
ckb <ckingbailey@gmail.com>
2020-04-11 20:21:36 -0700

    fix GOOGLE_CREDS env var

--- fetch_data/main.py
+++ fetch_data/main.py
@@ -4,7 +4,6 @@ from google.cloud import storage
 from dotenv import load_dotenv
 load_dotenv()
 import os
-from datetime import *
 
 url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
 gcp_project = "test-python-etl"
@@ -17,19 +16,15 @@ def fetch_data(input):
     print(text)
 
 def store_data():
-    # Instantiates a client
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket("bucket-data_raw")
-    bucket_csv = bucket.blob('data_us_nyt.csv')
-    bucket_csv.upload_from_string("placeholder")
-    print("complete!")
-
-# # Then do other things...
-# blob = bucket.get_blob('remote/path/to/file.txt')
-# print(blob.download_as_string())
-# blob.upload_from_string('New contents!')
-# blob2 = bucket.blob('remote/path/storage.txt')
-# blob2.upload_from_filename(filename='/local/path.txt')
+    try:
+        # Instantiates a client
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket("bucket-data_raw")
+        bucket_csv = bucket.blob('data_us_nyt.csv')
+        bucket_csv.upload_from_string("placeholder")
+        print("complete!")
+    except Exception as e:
+        print(str(e))
 
 def hello_pubsub(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
@@ -40,6 +35,4 @@ def hello_pubsub(event, context):
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     print("Message: " + pubsub_message)
 
-
-print("start at: " + str(datetime.now()))
 store_data()
